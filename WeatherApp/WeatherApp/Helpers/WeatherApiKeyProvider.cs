fe4f41fb3097fa590e5de741965946cd0056634d
Maxim Ermoshyn <emaxan1997@gmail.com>
2018-05-06 16:25:09 +0300

    Add keyProvider

---
+++ WeatherApp/WeatherApp/Helpers/WeatherApiKeyProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.Helpers
+{
+    class WeatherApiKeyProvider
+    {
+        public const String Key = "1835776f43559c5fefafaeacb03ea900";
+    }
+}
